2c85cc9ca4d6dac0c8292d5593996852e015c38e
nedim0101 <nedim.ejupovic10@gmail.com>
2025-08-29 01:26:21 +0200

    Add files via upload

---
+++ biblioteka-sistem 2/biblioteka-sistem/src/test/java/com/biblioteka/biblioteka_sistem/BibliotekaSistemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.biblioteka.biblioteka_sistem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotekaSistemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
